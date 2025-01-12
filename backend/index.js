787609674ed2c36e60f7b7b34cb8e93ab99cb230
Rushi <rushirpatil14@gmail.com>
2025-01-12 10:07:59 +0530

    updatedURL

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,44 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import hospRoute from './routes/HospitalRoute.js'
-import docRoute from "./routes/DocRoute.js"
+import hospRoute from './routes/HospitalRoute.js';
+import docRoute from './routes/DocRoute.js';
 
 const PORT = process.env.PORT || 5555;
-const mongoDBURL = 'mongodb+srv://rushirpatil491:Appleiphone@12@cluster0.ytarg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoDBURL = process.env.MONGODB_URI || 'mongodb+srv://rushirpatil491:Appleiphone%4012@cluster0.ytarg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const app = express();
 
-// Enable CORS for all routes and origins
-app.use(cors());
+// Enable CORS for specific origins or all
+app.use(cors({
+    origin: 'http://yourfrontend.com', // specify your frontend origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 
-// Middleware for parsing request body
+// Middleware for parsing JSON request bodies
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello, Express!");
 });
 
-app.use('/', hospRoute);
-app.use('/', docRoute);
+// Mount routes
+app.use('/hospitals', hospRoute);
+app.use('/doctors', docRoute);
 
-mongoose
-    .connect(mongoDBURL)
+// Connect to MongoDB
+mongoose.connect(mongoDBURL)
     .then(() => {
         console.log("Connected to MongoDB");
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log("Error connecting to MongoDB:", err));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
